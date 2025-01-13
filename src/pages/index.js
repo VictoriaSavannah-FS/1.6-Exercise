cd128ef98d943085f0ab17b7787bea9f0ae3ecf6
VictoriaSavannah-FS <169081831+VictoriaSavannah-FS@users.noreply.github.com>
2025-01-13 17:17:09 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,19 +21,19 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={360}
+          height={76}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            First Static Page{" "}
+            <code className="bg-black/[.80] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/pages/index.js
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Shall we have some fun?</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">

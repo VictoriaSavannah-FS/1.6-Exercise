3163d2af5fd5160e56dff1eb50ddae6b2590d70f
VictoriaSavannah-FS <169081831+VictoriaSavannah-FS@users.noreply.github.com>
2025-01-13 17:31:31 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https://www.ic3.gov/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -50,22 +50,22 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            IC3
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https://www.fbi.gov/history/brief-history"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Brief History
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="https://www.cfr.org/podcasts/spying-101"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -73,14 +73,14 @@ export default function Home() {
             aria-hidden
             src="/file.svg"
             alt="File icon"
-            width={16}
-            height={16}
+            width={32}
+            height={32}
           />
-          Learn
+          Espionage 101
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="https://www.fbi.gov/history/famous-cases/morris-worm"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -88,14 +88,14 @@ export default function Home() {
             aria-hidden
             src="/window.svg"
             alt="Window icon"
-            width={16}
-            height={16}
+            width={32}
+            height={32}
           />
-          Examples
+          Morris Worm Case
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="https://fbijobs.gov/stem/technology/computer-scientist#featured-openings"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -103,10 +103,10 @@ export default function Home() {
             aria-hidden
             src="/globe.svg"
             alt="Globe icon"
-            width={16}
-            height={16}
+            width={32}
+            height={32}
           />
-          Go to nextjs.org →
+          Go to FBI.org → Ready to Apply?
         </a>
       </footer>
     </div>
